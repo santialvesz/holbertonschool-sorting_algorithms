8599d9c73801d26ff6b774e35525014c911a4644
santialvesz <santialvesdsc@gmail.com>
2022-08-23 09:07:16 -0700

    Fixing the first case chaning the positive for the negatives

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,12 +9,12 @@ int a = 0, aux, num;
 	num = 0;
 		for (a = 1; (size_t)a < size; a++)
 		{
-			if (array[a] > array[a + 1])
+			if (array[a - 1] > array[a])
 			{
 				num = 1;
-				aux = array[a + 1];
-				array[a + 1] = array[a];
-				array[a] = aux;
+				aux = array[a];
+				array[a] = array[a - 1];
+				array[a - 1] = aux;
 				print_array(array, size);
 			}
 		}
