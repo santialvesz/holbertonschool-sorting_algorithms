e676eaff5cc2f42248486d955b334dacc3a8ac00
santialvesz <santialvesdsc@gmail.com>
2022-08-24 09:15:19 -0700

    Fixing betty in task 0 and task 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,29 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array
+ * @size: array size
+ */
 void bubble_sort(int *array, size_t size)
 {
-int a, aux, num;
+	int i = 0, aux, change;
 
-	while(1)
+	while (1)
 	{
-	num = 0;
-		for (a = 1; (size_t)a < size; a++)
+		change = 0;
+		for (i = 1; (size_t)i < size; i++)
 		{
-			if (array[a - 1] > array[a])
+			if (array[i - 1] > array[i])
 			{
-				num = 1;
-				aux = array[a];
-				array[a] = array[a - 1];
-				array[a - 1] = aux;
-				print_array(array, size);
+				change = 1;
+				aux = array[i];
+				array[i] = array[i - 1];
+				array[i - 1] = aux;
+				print_array((const int *)array, size);
 			}
 		}
-	if (num == 0) 
-		break;
+		if (change == 0)
+			break;
 	}
 
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,32 +1,34 @@
 #include "sort.h"
-/*
- *
-
+/**
+ * selection_sort - Function.
+ * @array: array.
+ * @size: size.
+ */
 void selection_sort(int *array, size_t size)
 {
-	int  aux;
-	size_t min, a, b;
+	size_t i, j, min;
+	int temp;
+
 
 	if (!array)
 		return;
-	for (a = 0; a < size - 1; a++)
-	{
-		min = a;
 
-		for (b = a + 1; b < size; b++)
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
 		{
-		
-				if(array[b] < array[min])
-				{
-					min = b;
-				}
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
 		}
-				if (min != a)
-				{
-					aux = array[a];
-					array[a] = array[min];
-					array[min] = aux;
-					print_array(array, size);
-				}		
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
 	}
 }
